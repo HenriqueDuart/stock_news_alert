4f5013d49be99b37ff37ca05ea9f200628ebbb9d
HenriqueDuarte <henrique.ribeiroduarte@gmail.com>
2026-02-08 12:14:07 +0100

    sec details

--- stock-news-alert/main.py
+++ stock-news-alert/main.py
@@ -1,16 +1,16 @@
 import requests
 import pandas as pd
 import os
+import smtplib
+from email.message import EmailMessage
 
 # Flags
 STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-STOCK_MOVE_THRESHOLD = 0.05
+COMPANY_NAME = "Tesla"
+STOCK_MOVE_THRESHOLD = 0.03
 
-
-
-#  API flags
-END_POINT = 'https://www.alphavantage.co/query'
+#  ALPHAV API flags
+ALPHAV_END_POINT = 'https://www.alphavantage.co/query'
 ALPHAV_API_KEY = os.environ.get('ALPHAV_API_KEY')
 ALPHA_PARAMS = {
     'function':'TIME_SERIES_DAILY',
@@ -19,14 +19,57 @@ ALPHA_PARAMS = {
     'apikey':ALPHAV_API_KEY
 }
 
+# NEWS API flags
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+NEWS_END_POINT = 'https://newsapi.org/v2/everything?'
+NEWS_PARAMS = {
+    'q':COMPANY_NAME,
+    'from':'2026-02-06',
+    'sortBy':'popularity',
+    'apiKey':NEWS_API_KEY
+}
 
+# Email details
+email_sender = 'john.dooe993@gmail.com'
+email_password = os.environ.get('JD_EMAIL_PASS') # No spaces
+email_receiver = ['john.dooe993@gmail.com']
+smtp_server = "smtp.gmail.com"
+port = 587
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def check_stock_move(dataframe, threshold):
+    dataframe = dataframe.sort_values('Date', ascending=False)
+    latest_change = dataframe['% change'].iloc[0]
+    if latest_change > threshold:
+        get_news()
+        # send_email(data)
+
+def get_news():
+    news_response = requests.get(url=NEWS_END_POINT, params=NEWS_PARAMS)
+    news_response.raise_for_status()
+    data = news_response.json()
+    top_3 = data['articles'][:3]
+    send_email(top_3)
+
+def send_email(content):
+    for item in content:
+        subject = item['title']
+        content = item['description']
+        final_content = content + '\n\n' + item['url']
+
+        msg = EmailMessage()
+        msg['Subject'] = f'>{STOCK_MOVE_THRESHOLD*100}% move in {STOCK} - ' + subject
+        msg['From'] = email_sender
+        msg['To'] = email_receiver
+        msg.set_content(final_content)
+
+        with smtplib.SMTP('smtp.gmail.com', port=port) as connection:
+            connection.starttls()
+            connection.login(user=email_sender, password=email_password)
+            connection.send_message(msg=msg)
+            print('Email sent successfully.')
 
 #  Getting the daily close prices for the stock on the last 100 business days
-response = requests.get(url=END_POINT, params=ALPHA_PARAMS)
+response = requests.get(url=ALPHAV_END_POINT, params=ALPHA_PARAMS)
 response.raise_for_status()
 data = response.json()
 
@@ -41,24 +84,5 @@ stock_df = stock_df.sort_values('Date', ascending=True)
 
 # Computing % change
 stock_df['% change'] = stock_df['Close price'].pct_change()
-stock_df = stock_df.sort_values('Date', ascending=False)
-print(stock_df.head(5))
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+check_stock_move(stock_df, STOCK_MOVE_THRESHOLD)
