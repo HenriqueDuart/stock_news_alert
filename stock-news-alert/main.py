bd149a86b2a2e95db4070bc2737e6c1afb4b976c
HenriqueDuarte <henrique.ribeiroduarte@gmail.com>
2026-02-08 10:54:30 +0100

    added stock data API call

--- stock-news-alert/main.py
+++ stock-news-alert/main.py
@@ -1,8 +1,44 @@
+import requests
+import pandas as pd
+
+# Flags
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STOCK_MOVE_THRESHOLD = 0.05
+
+
+
+#  API flags
+END_POINT = 'https://www.alphavantage.co/query'
+ALPHAV_API_KEY = 'DBJF0ET84OLVZLT3'
+ALPHA_PARAMS = {
+    'function':'TIME_SERIES_DAILY',
+    'symbol':STOCK,
+    'outputsize': 'compact',
+    'apikey':ALPHAV_API_KEY
+}
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+#  Getting the daily close prices for the stock on the last 100 business days
+response = requests.get(url=END_POINT, params=ALPHA_PARAMS)
+response.raise_for_status()
+data = response.json()
+
+# Put the data in a new dict with {key:value} formate where key=date and value=close_price
+time_series_dict = data['Time Series (Daily)']
+new_dict = {item:float(data['4. close']) for (item,data) in time_series_dict.items()}
+
+# Creating a pandas data frame, adjusting date to datetime format & make it sortable
+stock_df = pd.DataFrame(new_dict.items(), columns=['Date','Close price'])
+stock_df['Date'] = pd.to_datetime(stock_df['Date'])
+stock_df = stock_df.sort_values('Date', ascending=True)
+
+# Computing % change
+stock_df['% change'] = stock_df['Close price'].pct_change()
+stock_df = stock_df.sort_values('Date', ascending=False)
+print(stock_df.head(5))
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
